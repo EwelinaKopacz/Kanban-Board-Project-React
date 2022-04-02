400f2b74e46d81ecb3eda5481d6ea50c0307bcb6
Ewelina <ewelina@ekopacz.pl>
2022-04-02 12:36:22 +0200

    created environment to work

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+// ./src/components/App.js
+import React from 'react';
+import Box from './Box';
+import './components.css';
+
+const App = function () {
+    return <Box />;
+};
+
+export default App;
