c3e5499e7a8ce1bd30e43252f494ca0d6de538f7
Ewelina <ewelina@ekopacz.pl>
2022-04-05 13:12:00 +0200

    added context.js file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-// ./src/app.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
